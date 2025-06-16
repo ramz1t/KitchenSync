4569cbf2d361e101e7e22e993c7e769726200b77
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-06-16 18:09:55 +0200

    updated restaurant index

--- src/View/Restaurant/index.php
+++ src/View/Restaurant/index.php
@@ -10,16 +10,28 @@
 <body>
     <div class="container">
         <h1>-- <?= htmlspecialchars($restaurantName) ?> | KitchenSync --</h1>
-        <p class="info-p">-- Please choose one of the following options --</p>
+        <?php require __DIR__ . '/../Components/breadcrumbs.php'; ?>
+        <p class="info-p">Please choose one of the following options</p>
         <div class="restaurant-nav">
-            <a href="/restaurant/kitchen">For Kitchen</a>
             <a href="/restaurant/order">Ordering</a>
+            <a href="/restaurant/kitchen">For Kitchen</a>
             <a href="/restaurant/settings">Settings</a>
+            <a href="/restaurant/logs">Logs</a>
         </div>
         <p class="info-p">Or go back to the mainpage</p>
-        <form action="/logout" method="post">
-            <button type="submit" data-role="destructive">Logout</button>
-        </form>
+        <button id="logout-btn" type="submit" data-role="destructive">Logout</button>
+
+        <script>
+            function logout() {
+                if (confirm("Are you sure you want to log out?")) {
+                    document.cookie = "restaurant_pk=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    window.location.href = "/";
+                }
+            }
+            $(document).ready(function () {
+                $('#logout-btn').on('click', logout);
+            });
+        </script>
     </div>
 </body>
 
