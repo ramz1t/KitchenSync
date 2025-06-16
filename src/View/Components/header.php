9ba161ebd9f4f2721469b5a4e852e4e9f2d6a0fa
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-06-16 18:05:34 +0200

    updated header.php

--- src/View/Components/header.php
+++ src/View/Components/header.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8" />
-<script src="/js/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="/js/jquery-3.7.1.min.js"></script>
 <link rel="stylesheet" href="/styles/reset.css">
 <link rel="stylesheet" href="/styles/base.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
